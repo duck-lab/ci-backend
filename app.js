4084f4939400897ac7e3803da42e118a7bab1832
Oliver.W <ole3021@gmail.com>
2017-11-23 18:46:15 +0800

    replace with mongoose

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-require('dotenv').config();
+'use strict';
 
 module.exports = app => {
   app.beforeStart(async () => {
-    // Add leanCloud config to config object.
-    app.config.leanCloud = {
-      appId: process.env.LEANC_APPID || null,
-      appKey: process.env.LEANC_KEY || null,
-      masterKey: process.env.LEANC_MASTER_KEY || null
-    }
+    // TODO: add steps before start
   });
 };
